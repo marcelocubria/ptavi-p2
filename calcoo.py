26ccbaa6dbe28a9a9ff699b326f7cfadae5078a2
Marcelo Cubria <marcelocubria@gmail.com>
2018-09-27 09:41:44 +0200

    el punto 4 ya no almacena los valores y sigue el pep8

--- calcoo.py
+++ calcoo.py
@@ -3,28 +3,26 @@
 
 import sys
 
+
 class Calculadora():
-    def __init__(self, operador1, operador2):
-        self.operador1 = operador1
-        self.operador2 = operador2
+    def suma(self, operador1, operador2):
+        return (operador1 + operador2)
 
-    def suma(self):
-        return (self.operador1 + self.operador2)
+    def resta(self, operador1, operador2):
+        return (operador1 - operador2)
 
-    def resta(self):
-        return (self.operador1 - self.operador2)
 
 if __name__ == "__main__":
     try:
-        calculadora1 = Calculadora(int(sys.argv[1]), int(sys.argv[3]))
+        calculadora1 = Calculadora()
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
     if sys.argv[2] == "suma":
-        result = calculadora1.suma();
+        result = calculadora1.suma(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == "resta":
-        result = calculadora1.resta();
+        result = calculadora1.resta(int(sys.argv[1]), int(sys.argv[3]))
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
-    print("El resultado de", sys.argv[1], sys.argv[2], int(sys.argv[3]), "es igual a", result)
+    print("The result of, sys.argv[1], sys.argv[2], sys.argv[3], "is", result)
