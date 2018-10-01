f17736896ecbc74aaa6d8fec95381dda0f4a8814
Marcelo Cubria <marcelocubria@gmail.com>
2018-10-01 10:55:08 +0200

    esqueleto-oo sigue la guia pep8

--- esqueleto-oo.py
+++ esqueleto-oo.py
@@ -3,13 +3,14 @@
 
 
 class Clase(ClaseMadre):
-  "Esto es un ejemplo de clase que hereda de ClaseMadre"
+    "Esto es un ejemplo de clase que hereda de ClaseMadre"
 
-  def __init__(self, valor):
-    "Esto es el método iniciliazador"
-    self.atributo = valor
+    def __init__(self, valor):
+        "Esto es el método iniciliazador"
+        self.atributo = valor
+
 
 if __name__ == "__main__":
-  objeto = Clase("pepe") # Creo un objeto de la clase Clase
-                         # y le paso el valor pepe para su
+    objeto = Clase("pepe")  # Creo un objeto de la clase Clase
+# y le paso el valor pepe para su
 # atributo en la inicialización
